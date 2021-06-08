19d9b152da74215d501730ac250acdc7c381bc2c
plagocastro <plagocastro@danielcastelao.org>
2021-06-08 07:56:11 +0200

    ej1

--- src/commonMain/kotlin/sample/Sample.kt
+++ src/commonMain/kotlin/sample/Sample.kt
@@ -1,6 +1,9 @@
+import ej1 as ej1
+
 fun main() {
-
+    var contador = 0
+    for (cuenta in ej1(11).tabla7){
+        println("7 x $contador = $cuenta")
+        contador++
     }
-
-
 }
---
+++ src/commonMain/kotlin/sample/ej1.kt
@@ -0,0 +1,4 @@
+class ej1(size: Int) {
+    var contador = 0
+    var tabla7 = Array(size) { it * 7 }
+}
